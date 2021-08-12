98976bde1ced6b70d2f66697cc0e503add9abc57
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-08-12 18:53:08 +0300

    Improve Sentence class

--- src/main/java/com/epam/jwd/entity/Sentence.java
+++ src/main/java/com/epam/jwd/entity/Sentence.java
@@ -1,13 +1,18 @@
 package com.epam.jwd.entity;
 
+import java.util.List;
 import java.util.Objects;
 
 public class Sentence {
 
     private String sentence;
+    private List<Character> listOfPunctuationMarks;
+    private List<String> listOfWords;
 
-    public Sentence(String sentence) {
+    public Sentence(String sentence, List<Character> listOfPunctuationMarks, List<String> listOfWords) {
         this.sentence = sentence;
+        this.listOfPunctuationMarks = listOfPunctuationMarks;
+        this.listOfWords = listOfWords;
     }
 
     public String getSentence() {
@@ -18,17 +23,35 @@ public class Sentence {
         this.sentence = sentence;
     }
 
+    public List<Character> getListOfPunctuationMarks() {
+        return listOfPunctuationMarks;
+    }
+
+    public void setListOfPunctuationMarks(List<Character> listOfPunctuationMarks) {
+        this.listOfPunctuationMarks = listOfPunctuationMarks;
+    }
+
+    public List<String> getListOfWords() {
+        return listOfWords;
+    }
+
+    public void setListOfWords(List<String> listOfWords) {
+        this.listOfWords = listOfWords;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Sentence sentence1 = (Sentence) o;
-        return Objects.equals(sentence, sentence1.sentence);
+        return Objects.equals(sentence, sentence1.sentence)
+                && Objects.equals(listOfPunctuationMarks, sentence1.listOfPunctuationMarks)
+                && Objects.equals(listOfWords, sentence1.listOfWords);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(sentence);
+        return Objects.hash(sentence, listOfPunctuationMarks, listOfWords);
     }
 
     @Override
