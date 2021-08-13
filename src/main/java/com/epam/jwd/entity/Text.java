ed71b60f797947700fd4a80ac8dbd638c04db9de
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-08-13 15:34:26 +0300

    Create Text class(SyntaxStructureImpl)/CompositeClass

---
+++ src/main/java/com/epam/jwd/entity/Text.java
@@ -0,0 +1,66 @@
+package com.epam.jwd.entity;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Text implements SyntaxStructure{
+
+    private List<SyntaxStructure> listOfComponents;
+
+    public Text(SyntaxStructure... components) {
+        addComponent(components);
+    }
+
+    public List<SyntaxStructure> getListOfComponents() {
+        return listOfComponents;
+    }
+
+    public void setListOfComponents(List<SyntaxStructure> listOfComponents) {
+        this.listOfComponents = listOfComponents;
+    }
+
+    public void addComponent(SyntaxStructure component){
+        listOfComponents.add(component);
+    }
+
+    public void addComponent(SyntaxStructure... components){
+        listOfComponents.addAll(Arrays.asList(components));
+    }
+
+    public void addComponent(List<? extends SyntaxStructure> components){
+        listOfComponents.addAll(components);
+    }
+
+    public boolean removeComponent(SyntaxStructure component){
+        return listOfComponents.remove(component);
+    }
+
+    public int getNumOfComponents(){
+        return listOfComponents.size();
+    }
+
+    @Override
+    public void getStructure() {
+        listOfComponents.forEach(SyntaxStructure::getStructure);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Text text = (Text) o;
+        return Objects.equals(listOfComponents, text.listOfComponents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(listOfComponents);
+    }
+
+    @Override
+    public String toString() {
+        return listOfComponents.stream().toString();
+    }
+}
