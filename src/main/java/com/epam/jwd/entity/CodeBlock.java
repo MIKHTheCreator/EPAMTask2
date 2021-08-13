e3ba20e559d8fb21af5d3cb2593104ab9065e6af
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-08-13 15:12:19 +0300

    Create CodeBlock class(SyntaxStructureImpl)

---
+++ src/main/java/com/epam/jwd/entity/CodeBlock.java
@@ -0,0 +1,50 @@
+package com.epam.jwd.entity;
+
+import java.util.Objects;
+
+public class CodeBlock implements SyntaxStructure {
+
+    private String codeBlock;
+
+    public CodeBlock(String codeBlock) {
+        this.codeBlock = codeBlock;
+    }
+
+    public String getCodeBlock() {
+        return codeBlock;
+    }
+
+    public void setCodeBlock(String codeBlock) {
+        this.codeBlock = codeBlock;
+    }
+
+    public int getCodeBlockSize() {
+        return (int) codeBlock.lines().filter(s -> s.equals("\n")).count();
+    }
+
+    @Override
+    public void getStructure() {
+        System.out.print(codeBlock +"\s");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CodeBlock codeBlock1 = (CodeBlock) o;
+        return Objects.equals(codeBlock, codeBlock1.codeBlock);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codeBlock);
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder("'")
+                .append(codeBlock)
+                .append("'")
+                .toString();
+    }
+}
