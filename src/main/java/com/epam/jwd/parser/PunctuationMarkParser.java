711b229927941f2d3f8b2face9675251ee97ab2f
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-08-17 16:01:53 +0300

    Create PunctuationMarkParser

--- src/main/java/com/epam/jwd/parser/PunctuationMarkParser.java
+++ src/main/java/com/epam/jwd/parser/PunctuationMarkParser.java
@@ -1,4 +1,16 @@
 package com.epam.jwd.parser;
 
-public class PunctuationMarkParser implements Parser{
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PunctuationMarkParser {
+
+    private static final String PUNCTUATION_MARK_PATTERN = "[\\.,:;\\-+=!?\\{\\}\\(\\)\\[\\]\\/\\'\\\"\\>\\<\\%]";
+
+    public static String parseSentenceForPunctuationMark(String sentence){
+        Pattern pattern = Pattern.compile(PUNCTUATION_MARK_PATTERN);
+        Matcher matcher = pattern.matcher(sentence);
+
+        return matcher.find() ? matcher.group() : "";
+    }
 }
